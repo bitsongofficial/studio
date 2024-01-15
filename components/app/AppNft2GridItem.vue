1bc870d429db50657f5482018a03a06de78b95d0
angelorc <angelo@rcstart.it>
2024-01-15 18:57:46 +0100

    feat(drop-notification): add link to upcoming drop

--- components/app/AppNft2GridItem.vue
+++ components/app/AppNft2GridItem.vue
@@ -1,15 +1,25 @@
 <template>
   <v-card rounded="lg" width="230" color="black">
-    <v-img height="230" width="230" :src="img(data.image, { width: 230, height: 230, fit: 'cover' })"
-      gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.7)">
+    <NuxtLink to="/preview/ready-or-not" class="text-decoration-none text-white">
+      <v-img height="230" width="230" :src="img(data.image, { width: 230, height: 230, fit: 'cover' })"
+        gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.7)">
 
-      <div class="d-flex justify-space-between">
-        <v-chip>{{ date }}</v-chip>
-        <v-chip>{{ hour }}</v-chip>
-      </div>
-    </v-img>
-    <v-card-subtitle class="mt-2 text-caption">{{ data.subtitle }}</v-card-subtitle>
-    <v-card-title class="mt-n2">{{ data.title }}</v-card-title>
+        <div class="d-flex justify-space-between">
+          <v-chip>{{ date }}</v-chip>
+          <v-chip>{{ hour }}</v-chip>
+        </div>
+      </v-img>
+    </NuxtLink>
+    <v-card-subtitle class="mt-2 text-caption">
+      <NuxtLink to="/preview/ready-or-not" class="text-decoration-none text-white">
+        {{ data.subtitle }}
+      </NuxtLink>
+    </v-card-subtitle>
+    <v-card-title class="mt-n2">
+      <NuxtLink to="/preview/ready-or-not" class="text-decoration-none text-white">
+        {{ data.title }}
+      </NuxtLink>
+    </v-card-title>
     <AppDropNotificationBtn drop-id="ready_or_not" :title="data.title" :subtitle="data.subtitle" :image="data.image"
       :start-time="data.startTime" />
   </v-card>
