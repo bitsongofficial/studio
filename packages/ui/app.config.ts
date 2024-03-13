b410f450370476568eae85adbee81436285196f2
angelorc <angelo@rcstart.it>
2024-03-13 15:04:26 +0100

    Update UI configuration and add NavigationDrawer component

--- packages/ui/app.config.ts
+++ packages/ui/app.config.ts
@@ -1,14 +1,39 @@
 export default defineAppConfig({
-  myLayer: {
-    name: 'Hello from Nuxt layer'
-  }
+  app: {
+    name: 'BitSong Studio',
+    network: 'mainnet',
+    githubRepo: 'bitsongofficial/studio',
+  },
+  socials: [
+    {
+      icon: ['fab', 'discord'],
+      href: 'https://discord.bitsong.io',
+    },
+    {
+      icon: ['fab', 'twitter'],
+      href: 'https://twitter.com/BitSongOfficial',
+    },
+    {
+      icon: ['fab', 'telegram'],
+      href: 'https://t.me/BitSongOfficial',
+    },
+    {
+      icon: ['fab', 'instagram'],
+      href: 'https://www.instagram.com/bitsong_official'
+    },
+  ]
 })
 
 declare module '@nuxt/schema' {
   interface AppConfigInput {
-    myLayer?: {
-      /** Project name */
-      name?: string
-    }
+    app?: {
+      name?: string,
+      network?: string,
+      githubRepo?: string,
+    },
+    socials?: {
+      icon: [string, string],
+      href: string,
+    }[]
   }
 }
--- packages/ui/app.vue
+++ packages/ui/app.vue
@@ -1,3 +1,3 @@
 <template>
-  <HelloWorld />
+  <BHelloWorld />
 </template>
